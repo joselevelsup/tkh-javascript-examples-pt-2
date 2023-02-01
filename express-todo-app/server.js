8cb943938f955df543e4fab9bb0456aa1af57224
joselevelsup <joselevelsup@gmail.com>
2023-02-01 12:42:15 -0500

    changing route structure

--- express-todo-app/server.js
+++ express-todo-app/server.js
@@ -1,6 +1,9 @@
 import express from "express"; //Import Express
 import { Low } from "lowdb"; //Import the LowDB module. Uses a JSON file to create our "database"
 import { JSONFile } from "lowdb/node";
+import { nanoid } from "nanoid";
+
+import setupTodoRouter from "./routes/todo.js";
 
 (async function () {
   // Configure lowdb to write to JSONFile. This will be our "database"
@@ -22,30 +25,19 @@ import { JSONFile } from "lowdb/node";
   //Use Builtin middleware to extract JSON data from the body of any request made to the server
   app.use(express.json());
 
-  //Create our GET route that just sends back the Todos data
-  app.get("/", function (_request, response) {
-    //The underscore means to ignore the param that's not being used
-    response.status(200).json({
-      //Set our response to have a status of 200 (OK!) and to respond with JSON
-      success: true,
-      todos: db.data.todos, //Returns the todos from our DB
-    });
-  });
+  app.use("/todo", function(request, response, next){
+    if(request.query.admin === "true"){
+      next();
+    } else {
+      response.status(401).json({
+        success: false
+      });
+    }
+  })
 
-  app.post("/", function (request, response) {
-    //Push the new todo
-    db.data.todos.push({
-      name: request.body.todo,
-    });
-
-    //Save the todo to the "database"
-    db.write();
-
-    //Respond with 200 (OK!) and tell the user the request is successful
-    response.status(200).json({
-      success: true,
-    });
-  });
+  // "/" => "/todo"
+  // "/:todo" => "/todo/:todo"
+  app.use("/todo", setupTodoRouter(db));
 
   //Have the app listen on port 8080
   app.listen(8080, function () {
